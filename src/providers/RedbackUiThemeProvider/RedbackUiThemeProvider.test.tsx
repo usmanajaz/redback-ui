45ad73a7bed18b63d95d902382c6ca329c9d817f
doubleedesign <leesa@doubleedesign.com.au>
2024-04-14 10:59:30 +1000

    Test: Fix test

--- src/providers/RedbackUiThemeProvider/RedbackUiThemeProvider.test.tsx
+++ src/providers/RedbackUiThemeProvider/RedbackUiThemeProvider.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import RedbackUiThemeProvider from './RedbackUiThemeProvider';
+import { themes } from '../../themes';
 
 describe('<RedbackUiThemeProvider />', () => {
 	it('should mount', () => {
-		render(<RedbackUiThemeProvider/>);
+		render(<RedbackUiThemeProvider theme={themes.default}>Content</RedbackUiThemeProvider>);
 
 		const redbackUiThemeProvider = screen.getByTestId('RedbackUiThemeProvider');
 
