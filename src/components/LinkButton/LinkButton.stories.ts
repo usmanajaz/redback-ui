1937f581ffe83e8ad47367b198be4be3ac3594aa
doubleedesign <leesa@doubleedesign.com.au>
2024-04-14 10:57:15 +1000

    Docs: Don't include default props; fix template generation path; minor wording tweaks

--- src/components/LinkButton/LinkButton.stories.ts
+++ src/components/LinkButton/LinkButton.stories.ts
@@ -1,6 +1,5 @@
 import LinkButton from './LinkButton';
 import type { Meta, StoryObj } from '@storybook/react';
-import { ThemeColor, ThemeElementAppearance } from '../../types.ts';
 import { themeColorSubset } from '../../types.ts';
 
 const meta = {
@@ -24,13 +23,6 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-
-const defaultProps = {
-	href: '#',
-	color: 'primary' as ThemeColor,
-	appearance: 'solid' as ThemeElementAppearance
-};
-
 const disableControls = {
 	parameters: {
 		controls: {
@@ -45,14 +37,12 @@ const disableControls = {
 export const Demo: Story = {
 	args: {
 		label: 'LinkButton',
-		...defaultProps
 	},
 	tags: ['excludeFromSidebar']
 };
 
 export const Default: Story = {
 	args: {
-		...defaultProps,
 		label: 'Default LinkButton',
 	},
 	...disableControls
