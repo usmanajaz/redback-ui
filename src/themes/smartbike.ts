443c70d08639840406066b261b7301b391efa7f4
doubleedesign <leesa@doubleedesign.com.au>
2024-04-14 22:46:35 +1000

    Design: Add some draft themes for some of the other projects

---
+++ src/themes/smartbike.ts
@@ -0,0 +1,16 @@
+import defaultTheme from './default';
+
+const smartbike = {
+	...defaultTheme,
+	colors: {
+		...defaultTheme.colors,
+		primary: '#fbb653',
+		secondary: '#e97462',
+		success: '#599d62',
+		info: '#5d8de8',
+		background: '#f8f9fa',
+		dark: '#370e4a',
+	},
+};
+
+export default smartbike;
